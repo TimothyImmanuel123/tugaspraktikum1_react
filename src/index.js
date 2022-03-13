5bd96c76e5ab2b484f8bbc6b116fc2d2041c132e
Timothyimmanuel <timothy_immanuel_29rpl@student.smktelkom-mlg.sch.id>
2022-03-13 21:00:04 +0700

    praktikum1

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Bmi from "./pages/bmi";
+import Bank from "./pages/bank";
+import Binary from "./pages/binary";
+import Final from "./pages/final";
+import Octal from "./pages/octal";
+import Decimal from "./pages/decimal";
+import Hexadecimal from "./pages/hexadecimal";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Decimal/>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
